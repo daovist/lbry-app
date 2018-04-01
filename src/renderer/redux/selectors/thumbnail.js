cfa6e1e89213a72fc4e6e29ccf4b24d5cdb9d0d7
Travis Eden <daovist@gmail.com>
2018-04-01 17:36:09 -0400

    add selectors

---
+++ src/renderer/redux/selectors/thumbnail.js
@@ -0,0 +1,11 @@
+import { createSelector } from 'reselect';
+
+const selectState = state => state.upload || {};
+
+export const selectThumbnailApi = createSelector(selectState, state => state.api);
+
+export const selectThumbnailStatus = createSelector(selectState, state => state.status);
+
+export const selectThumbnailUrl = createSelector(selectState, state => state.url);
+
+export const selectThumbnailNsfw = createSelector(selectState, state => state.nsfw);
